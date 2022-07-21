9106c099fa2a866c77dd1ad1d1bd09ba3e0a7b0f
bielrossi15 <bielrossiborba@gmail.com>
2022-07-21 19:37:45 -0300

    Adicionando funções para cálculo de informação

--- d_tree/decision_tree.py
+++ d_tree/decision_tree.py
@@ -18,7 +18,7 @@ class DecisionTree:
             class_entropy = 0
             if classes_number != 0:
                 class_entropy = -(classes_number/train_data.shape[0]) * np.log2(classes_number/train_data.shape[0])
-            entropy += class_entropy
+            total_entropy += class_entropy
         return total_entropy
 
     def _feature_entropy(self, feature_data, classes):
@@ -30,3 +30,31 @@ class DecisionTree:
                 class_entropy = -(classes_number/feature_data.shape[0]) * np.log2(classes_number/feature_data.shape[0])
             feature_entropy += class_entropy
         return feature_entropy
+    
+    def _information_gain_per_feature(self, feature, classes, train_data):
+        f_list = train_data[feature].unique()
+        feature_information = .0
+        train_data_size = train_data.shape[0]
+
+        for f in f_list:
+            f_value_data = train_data[train_data[feature] == f]
+            f_value_count = f_value_data.shape[0]
+            f_value_entropy = self._feature_entropy(f_value_data, classes)
+            feature_information += (f_value_count /
+                                    train_data_size) * f_value_entropy
+
+        entropy = self._dataset_entropy(classes, train_data)
+        return entropy - feature_information
+
+    def _most_info(self, classes, train_data):
+        f_list = train_data.columns.drop(self.label)
+        max_info = -1
+        max_feature = None
+
+        for f in f_list:
+            f_gain = self._information_gain_per_feature(f, classes, train_data)
+            if max_info < f_gain:
+                max_info = f_gain
+                max_feature = f
+        return max_feature, max_info
+
